5678d423753dacd8b79d6bff1ef5554de0ec75e7
Guilherme Mendes <guilherme8787@gmail.com>
2024-08-22 00:18:44 -0300

    feat: add seeders to new contas table

---
+++ database/factories/ContaFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Conta;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Conta>
+ */
+class ContaFactory extends Factory
+{
+    protected $model = Conta::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'numero_conta' => $this->faker->unique()->numberBetween(100000, 999999),
+            'saldo' => $this->faker->randomFloat(2, 100, 10000),
+        ];
+    }
+
+    public function withAttributes(array $attributes = [])
+    {
+        return $this->state(function (array $attributes) {
+            return array_merge([
+                'numero_conta' => $this->faker->unique()->numberBetween(100000, 999999),
+                'saldo' => $this->faker->randomFloat(2, 0, 10000),
+            ], $attributes);
+        });
+    }
+}
