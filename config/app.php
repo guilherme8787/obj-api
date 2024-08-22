24087ee695472c7a684d4fdb103dd5856cf1ded5
Guilherme Mendes <guilherme8787@gmail.com>
2024-08-22 01:03:54 -0300

    feat: register new conta and transacao provider in config

--- config/app.php
+++ config/app.php
@@ -169,6 +169,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\TransactionServiceProvider::class,
+        App\Providers\ContaRepositoryProvider::class,
+        App\Providers\TransacaoRepositoryProvider::class,
     ])->toArray(),
 
     /*
