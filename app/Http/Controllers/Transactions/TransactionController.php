074ce2bb61d13a33fd9f368e392013e0fd5ce123
Guilherme Mendes <guilherme8787@gmail.com>
2024-08-22 01:45:08 -0300

    feat: make controller io only

---
+++ app/Http/Controllers/Transactions/TransactionController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Transactions;
+
+use App\Exceptions\InsufficientBalanceException;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TransactionRequest;
+use App\Services\Transactions\Contracts\TransactionServiceContract;
+use Exception;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+
+class TransactionController extends Controller
+{
+    public function __construct(
+        private TransactionServiceContract $transactionService
+    ) {
+    }
+
+    public function __invoke(TransactionRequest $request)
+    {
+        $data = $request->validated();
+
+        try {
+            $accountStatus = $this->transactionService->handleTransaction($data);
+
+            return response()->json($accountStatus, Response::HTTP_CREATED);
+        } catch (InsufficientBalanceException $insufficientBalance) {
+            Log::info($insufficientBalance->getMessage(), [
+                'data' => $data,
+                'exception' => $insufficientBalance,
+                'code' => 'transaction_error_no_balance'
+            ]);
+
+            return response()->json(
+                [
+                    'message' => $insufficientBalance->getMessage()
+                ],
+                Response::HTTP_NOT_FOUND
+            );
+        } catch (Exception $exception) {
+            Log::critical($exception->getMessage(), [
+                'data' => $data,
+                'exception' => $exception,
+                'code' => 'transaction_error_critical_exception'
+            ]);
+
+            abort(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
